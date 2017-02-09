a483c5d197a06e40332adeb6f9c9afc149523ecc
mechaxl <mechaxl@gmail.com>
2017-02-08 23:01:06 -0600

    Updating for cargo templating

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 extern crate amethyst;
 
-use amethyst::engine::{Application, Duration, State, Trans};
+use amethyst::engine::{Application, State, Trans};
+use amethyst::specs::World;
+use amethyst::gfx_device::DisplayConfig;
+use amethyst::asset_manager::AssetManager;
+use amethyst::renderer::Pipeline;
 
 struct HelloWorld;
 
 impl State for HelloWorld {
-    fn on_start(&mut self) {
+    fn on_start(&mut self, _: &mut World, _: &mut AssetManager, _: &mut Pipeline) {
         println!("Game started!");
     }
 
-    fn update(&mut self, _delta: Duration) -> Trans {
+    fn update(&mut self, _: &mut World, _: &mut AssetManager, _: &mut Pipeline) -> Trans {
         println!("Hello from Amethyst!");
         Trans::Quit
     }
 
-    fn on_stop(&mut self) {
+    fn on_stop(&mut self, _: &mut World, _: &mut AssetManager, _: &mut Pipeline) {
         println!("Game stopped!");
     }
 }
 
 fn main() {
-    let mut game = Application::new(HelloWorld);
+    let display_config = DisplayConfig::default();
+    let mut game = Application::build(HelloWorld, display_config).done();
     game.run();
 }
